aa1134b24124a3092ce436c27ca7a8e508f48a76
Mohannad <supreme7121@gmail.com>
2023-09-22 14:01:25 -0400

    BuddyInfo added to AddressBook

--- src/AddressBook.java
+++ src/AddressBook.java
@@ -21,7 +21,9 @@ public class AddressBook {
     }
 
     public static void main(String[] args){
-        System.out.println("Address Book");
-
+        BuddyInfo buddy = new BuddyInfo ("Tom", "Carleton", "(613)");
+        AddressBook buddies = new AddressBook();
+        buddies.addBuddy(buddy);
+        buddies.removeBuddy(buddy);
     }
 }
